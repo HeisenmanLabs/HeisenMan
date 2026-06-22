b8d21fa49416b8cd0659522a62205b8ded408426
HeisenmanLabs <helloheisenman@gmail.com>
2026-06-22 20:11:31 +0530

    Support root base path on Render as well as Netlify

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NETLIFY ? "/" : "/HeisenMan/",
+  base: (process.env.NETLIFY || process.env.RENDER) ? "/" : "/HeisenMan/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
